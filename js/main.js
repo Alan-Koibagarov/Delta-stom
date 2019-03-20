62bf00621f4c2be09c7aaa7fb4c73b06fb8acd55
Alan-Koibagarov <36890792+Alan-Koibagarov@users.noreply.github.com>
2019-03-20 15:20:04 +0600

    new

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    $(".block1_work .owl-carousel").owlCarousel({
+        items: 4,
+        autoWidth: true,
+        margin: 30,
+        loop: true,
+        dotsEach: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        center: true,
+    });
+
+});
+
+$(document).ready(function () {
+
+    $(".block3 .owl-carousel").owlCarousel({
+        items: 3,
+        autoWidth: true,
+        margin: 100,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        center: true,
+    });
+
+});
---
+++ js/slider_block4.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $(".slider_one").owlCarousel({
+        items: 4,
+        autoWidth: true,
+        margin: 30,
+        loop: true,
+        dotsEach: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+    });
+
+});
